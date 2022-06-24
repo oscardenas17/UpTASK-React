9c4d72cbbc1edd40f20ce012acf1b13bdca482d1
oscardenas17 <oscardenas17@gmail.com>
2022-06-23 22:35:33 -0500

    configurando Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+     <h1>ey arnold</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./src/**/*¨.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
