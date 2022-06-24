6b6c3c6ec24d721433f94b13dbf50bba583f61ba
oscardenas17 <oscardenas17@gmail.com>
2022-06-23 23:41:51 -0500

    LayoutPrincipal Area publica

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,14 +1,15 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
   return (
     <>
-    <div> authLayout</div>
-        <Outlet>
-
-        </Outlet>
+      <main className="container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center">
+        <div className="md:w-2/3 lg:1/2">
+          <Outlet />
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
