3337c7dba921970b91fd4956d1649ac96531ee28
oscardenas17 <oscardenas17@gmail.com>
2022-06-24 00:15:14 -0500

    Creando el formulario de autenticación

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,44 @@
-
 const Login = () => {
   return (
-    <div>login</div>
-  )
-}
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Inicia Sesión y administra tus{" "}
+        <span className="text-slate-700">proyectos</span>
+      </h1>
 
-export default Login
+      <form action="" className="my-10 bg-white shadow rounded-lg px-5 py-10">
+        <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="email">Email</label>
+          <input 
+            id="email"
+            type="email"
+            name="email" 
+            placeholder="Email de registro" 
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="password">Password</label>
+          <input 
+            id="password"
+            type="password"
+            name="password" 
+            placeholder="Password de registro" 
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+
+        <input type="submit" 
+            value="Iniciar Sesión"
+            className="bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transitios-colors"
+        />
+
+
+      </form>
+    </>
+  );
+};
+
+export default Login;
