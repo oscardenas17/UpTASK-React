66cb9f35fab55d7ebf94dabde3f271da0dc4145f
oscardenas17 <oscardenas17@gmail.com>
2022-06-24 09:45:32 -0500

    Routing a paginas publicas- olvide y recup passw

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,5 @@
+import {Link} from 'react-router-dom'
+
 const Login = () => {
   return (
     <>
@@ -6,7 +8,7 @@ const Login = () => {
         <span className="text-slate-700">proyectos</span>
       </h1>
 
-      <form action="" className="my-10 bg-white shadow rounded-lg px-5 py-10">
+      <form action="" className="my-10 bg-white shadow rounded-lg p-10">
         <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="email">Email</label>
           <input 
@@ -35,6 +37,17 @@ const Login = () => {
             className="bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transitios-colors"
         />
 
+        <nav className='lg:flex lg:justify-between'>
+          <Link
+            className='block text-center my-5 text-slate-500 uppercase text-sm'
+            to="/registrar"
+          >Regístrate</Link>
+
+          <Link
+            className='block text-center my-5 text-slate-500 uppercase text-sm'
+            to="/olvide-password"
+          >Olvide Mi Contraseña</Link>
+        </nav>
 
       </form>
     </>
