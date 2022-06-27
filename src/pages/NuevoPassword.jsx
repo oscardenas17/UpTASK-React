6a21010594a76d075c2276085ffb3ae5dcfe1a26
oscardenas17 <oscardenas17@gmail.com>
2022-06-27 12:01:37 -0500

    Validar Token para resetear la contraseña

--- src/pages/NuevoPassword.jsx
+++ src/pages/NuevoPassword.jsx
@@ -1,6 +1,41 @@
-import {Link} from 'react-router-dom'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import {Link, useParams} from 'react-router-dom'
+import axios from 'axios'
+import Alerta from '../components/Alerta'
+
 
 const NuevoPassword = () => {
+
+  const params = useParams();
+  //console.log(params)
+  const {token} =params
+
+  const [tokenValido, setTokenValido] = useState(false);
+  const [alerta, setAlerta] = useState( {} )
+
+useEffect(() => {
+
+  const comprobarToken = async ()=>{
+    try {
+      const {data} = await axios.get(`http://localhost:4000/api/usuarios/olvide-password/${token}`)
+      setTokenValido(true) //si token es válido, muestra el formulario
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error:true,
+    })
+    }
+  };
+
+  comprobarToken();
+}, []);
+
+const {msg} = alerta;
+
+  
+  
+
   return (
     <>
     <h1 className="text-sky-600 font-black text-6xl capitalize">
@@ -8,27 +43,32 @@ const NuevoPassword = () => {
       <span className="text-slate-700">proyectos</span>
     </h1>
 
-    <form action="" className="my-10 bg-white shadow rounded-lg p-10">
+    {msg  && <Alerta alerta={alerta}/>}
+
+{/* Token valido */}
+   { tokenValido && (
+       <form action="" className="my-10 bg-white shadow rounded-lg p-10">
 
     
-      <div className="my-5">
-          <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="password">Nueva Contraseña</label>
-        <input 
-          id="password"
-          type="password"
-          name="password" 
-          placeholder="Escribe tu nueva contraseña" 
-          className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-        />
-      </div>
-
-  
-      <input type="submit" 
-          value="Guardar Nuevo Password"
-          className="bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transitios-colors"
-      />     
-
-    </form>
+       <div className="my-5">
+           <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="password">Nueva Contraseña</label>
+         <input 
+           id="password"
+           type="password"
+           name="password" 
+           placeholder="Escribe tu nueva contraseña" 
+           className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+         />
+       </div>
+ 
+   
+       <input type="submit" 
+           value="Guardar Nuevo Password"
+           className="bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transitios-colors"
+       />     
+ 
+     </form>
+   )}
 
     <nav className='lg:flex lg:justify-between'>
         <Link
