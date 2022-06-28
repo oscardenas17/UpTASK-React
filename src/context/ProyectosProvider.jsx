66920c02116e297dbb8716cd5d04ea4217020591
oscardenas17 <oscardenas17@gmail.com>
2022-06-28 13:13:46 -0500

    Creando el Hook para el context de proyectos

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -5,10 +5,13 @@ import clienteAxios from '../config/clienteAxios'
 const ProyectosContext = createContext();
 
 const ProyectosProvider = ( {children} ) => {
+
+  const [proyectos, setProyectos] = useState([]);
+
   return (
     <ProyectosContext.Provider 
       value={{ 
-
+        proyectos
        }}
     >
       {children}
@@ -20,4 +23,4 @@ export {
   ProyectosProvider
 }
 
-export default ProyectosProvider
+export default ProyectosContext
--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -1,6 +1,9 @@
-
+import useProyectos from "../hooks/useProyectos"
 
 const Proyectos = () => {
+
+  const {proyectos} = useProyectos()
+  console.log(proyectos);
   return (
     <>
       <h1 className="text-4xl font-black">Proyectos</h1>
