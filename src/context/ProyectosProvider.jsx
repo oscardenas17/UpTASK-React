65614b9d8d598c7768f62f7bc06cd88a97531dcc
oscardenas17 <oscardenas17@gmail.com>
2022-07-20 13:58:35 -0500

    Almacenando tarea en la base de datos

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -1,152 +1,155 @@
-import { useState, useEffect, createContext } from "react"
-import clienteAxios from '../config/clienteAxios'
-import {useNavigate} from 'react-router-dom'
+import { useState, useEffect, createContext } from "react";
+import clienteAxios from "../config/clienteAxios";
+import { useNavigate } from "react-router-dom";
 
 //Crearl el Context
 const ProyectosContext = createContext();
 
-const ProyectosProvider = ( {children} ) => {
-
+const ProyectosProvider = ({ children }) => {
   const [proyectos, setProyectos] = useState([]);
   const [alerta, setAlerta] = useState({});
   const [proyecto, setProyecto] = useState({});
   const [cargando, setCargando] = useState(false);
 
-  const [modalFormularioTarea, setModalFormularioTarea] = useState(false);  
+  const [modalFormularioTarea, setModalFormularioTarea] = useState(false);
 
   const navigate = useNavigate();
-
 
   //Obtener proyectos para listado
   useEffect(() => {
     const obtenerProyectos = async () => {
-     
       try {
         //EXTRAER TOKENES
-        const token = localStorage. getItem('token');
-        if(!token){return} 
+        const token = localStorage.getItem("token");
+        if (!token) {
+          return;
+        }
 
         const config = {
-          headers: { 
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
           },
         };
-        //traer proyectos 
-        const {data} = await clienteAxios('/proyectos', config)
+        //traer proyectos
+        const { data } = await clienteAxios("/proyectos", config);
         //console.log(data);
         //colocar en el state los proyectos, usamos el setproyectos
-        setProyectos(data)
+        setProyectos(data);
       } catch (error) {
         console.log(error);
       }
-    }
-    //return  ()=> 
-     obtenerProyectos()
-
+    };
+    //return  ()=>
+    obtenerProyectos();
   }, []);
 
-  const mostrarAlerta= (alerta)=>{
-    setAlerta(alerta)
+  const mostrarAlerta = (alerta) => {
+    setAlerta(alerta);
 
-    setTimeout(() => { //elimina la alerta despues de 5 s
-      setAlerta({})
+    setTimeout(() => {
+      //elimina la alerta despues de 5 s
+      setAlerta({});
     }, 5000);
-  }
+  };
 
   //Interactuar con la API para consumir y enviar data de proyectos
-  const submitProyecto= async proyecto =>{
+  const submitProyecto = async (proyecto) => {
     //console.log(proyecto); //datos que vienen desde el formProyecto
 
     //mostrar proyecto creado en la lista, copia proyectos actuales con ...proyectos y se añade otro con data
-  
+
     try {
       //EXTRAER TOKENES
-      const token = localStorage. getItem('token');
-      if(!token){return} 
+      const token = localStorage.getItem("token");
+      if (!token) {
+        return;
+      }
 
       const config = {
-        headers: { 
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-         },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
       };
 
-      const {data} = await clienteAxios.post('/proyectos', proyecto, config)
+      const { data } = await clienteAxios.post("/proyectos", proyecto, config);
       //console.log(data)
-      setProyectos([...proyectos, data])
+      setProyectos([...proyectos, data]);
       setAlerta({
-        msg: 'Proyecto Creado',
-        error: false
+        msg: "Proyecto Creado",
+        error: false,
       });
 
       //redireccionar despues de creado el proyecto
       setTimeout(() => {
-        setAlerta({})
-        navigate('/proyectos')
+        setAlerta({});
+        navigate("/proyectos");
       }, 3000);
     } catch (error) {
       console.log(error); //
     }
   };
 
-
   //Obtener informacion de un proyecto por su ID - hacer disponible en proyecto para llamarlo
   const obtenerProyecto = async (id) => {
     //console.log(id);
-    setCargando(true)
+    setCargando(true);
     try {
-      const token = localStorage. getItem('token');
-      if(!token){
-        return
-      } 
+      const token = localStorage.getItem("token");
+      if (!token) {
+        return;
+      }
 
       const config = {
-        headers: { 
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-         },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
       };
 
-      const {data} = await clienteAxios(`/proyectos/${id}`, config)
+      const { data } = await clienteAxios(`/proyectos/${id}`, config);
       //Mostrando Informacion de un proyecto
-      setProyecto(data) 
+      setProyecto(data);
       console.log(data); //coin esto hacer disponible proyecto en el provider
       console.log(proyecto);
     } catch (error) {
       console.log(error); //
-    }finally{
-      setCargando(false)  //pasar el cargando disponible para los demas pages
+    } finally {
+      setCargando(false); //pasar el cargando disponible para los demas pages
     }
-  }
+  };
 
+  //Pasar modal
+  const handleModalTarea = () => {
+    setModalFormularioTarea(!modalFormularioTarea);
+  };
 
-  //Pasar modal 
-  const handleModalTarea =  () => { 
-    setModalFormularioTarea(!modalFormularioTarea)
-  }
+  //SubmitTarea modal
+  const submitTarea = async (tarea) => {
+    console.log(tarea);
+  };
 
   return (
-    <ProyectosContext.Provider 
-      value={{ 
+    <ProyectosContext.Provider
+      value={{
         proyectos,
         mostrarAlerta,
         alerta,
         submitProyecto,
-        obtenerProyecto,//para llamarla en proyecto
+        obtenerProyecto, //para llamarla en proyecto
         proyecto, //mostrar informacion de un proyecto, se extare en proyecto por el useProyectos
-        cargando,   //se extrae en proyecto
+        cargando, //se extrae en proyecto
         modalFormularioTarea, //leer si esta true o false
-        handleModalTarea
+        handleModalTarea,
+        submitTarea,
       }}
     >
       {children}
-    </ProyectosContext.Provider >
-  )
-}
+    </ProyectosContext.Provider>
+  );
+};
 
-export { 
-  ProyectosProvider
-}
+export { ProyectosProvider };
 
-export default ProyectosContext
+export default ProyectosContext;
